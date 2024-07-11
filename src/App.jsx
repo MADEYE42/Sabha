c3820b72e1b821a3136ca7fc14154a7afd864d47
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-11 18:32:15 +0530

    Routing Done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,18 @@ import { useState } from 'react'
 import './App.css'
 import HomeScreen from './Screens/HomeScreen';
 import { ThemeContext } from '../src/context/ThemeContext';
-
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import AddNewScreen from './Screens/AddNewScreen';
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element:<HomeScreen/>
+  },
+  {
+    path:'/new',
+    element:<AddNewScreen/>
+  }
+])
 function App() {
   const [theme, setTheme] = useState('winter')
 
@@ -10,7 +21,7 @@ function App() {
     <ThemeContext.Provider value={{theme,setTheme}}>
     <div className='flex flex-col items-center p-4 md:p-10' data-theme={theme} >
       <div className='max-w-2xl w-full items-center'>
-        <HomeScreen/>
+        <RouterProvider router={router}/>
       </div>      
     </div>
     </ThemeContext.Provider>
---
+++ src/Screens/AddNewScreen.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function AddNewScreen() {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default AddNewScreen
