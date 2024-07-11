4cf46ec7455f0e96d927b5428d434178759e5918
Gouresh  Madye <117588160+MADEYE42@users.noreply.github.com>
2024-07-11 19:15:18 +0530

    Database Connected

---
+++ utils/index.js
@@ -0,0 +1,5 @@
+import { neon } from '@neondatabase/serverless';
+import { drizzle } from 'drizzle-orm/neon-http';
+import * as schema from './schema.js'
+const sql = neon("postgresql://neondb_owner:MtsXBR1r5OAN@ep-summer-bonus-a14137d8.ap-southeast-1.aws.neon.tech/ai-content?sslmode=require");
+export const db = drizzle(sql,{schema});
